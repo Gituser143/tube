4870e1ed29751267d7dc22158d011acd5f8545ae
Gituser143 <bhargavsnv100@gmail.com>
2021-04-07 01:35:10 +0530

    Add Skeleton models

--- oyt_python/oyt/models.py
+++ oyt_python/oyt/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    path = models.CharField(max_length=100)
+    datetime = models.DateField(blank=False, null=False)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=300)
+    datetime = models.DateField(blank=False, null=False)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
--- oyt_python/oyt_python/settings.py
+++ oyt_python/oyt_python/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'oyt',
     'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
